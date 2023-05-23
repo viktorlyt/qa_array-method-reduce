7b9934b0e5bfe2c27c5451ac19d97b459d3e5be1
viktorlyt <viktorlyt@gmail.com>
2023-05-24 01:05:44 +0300

    done

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -11,9 +11,56 @@ describe('reduce', () => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {
+  it('should call a cb once per item', () => {
+    const items = [1, 2, 3];
+    const f = jest.fn();
 
+    items.reduce2(f, 1);
+    expect(f).toHaveBeenCalledTimes(3);
   });
 
-  // Add tests here
+  it('no prev', () => {
+    const items = [1, 2, 3];
+    const f = jest.fn();
+
+    items.reduce2(f);
+    expect(f).toHaveBeenCalledTimes(2);
+  });
+
+  it('should call a cb for an empty array', () => {
+    const f = jest.fn();
+
+    [].reduce2(f);
+    expect(f).not.toHaveBeenCalled();
+  });
+
+  it('should pass an el, an index and an array to a cb', () => {
+    const f = jest.fn();
+
+    const items = [100, 200, 300];
+
+    items.reduce2(f);
+
+    expect(f).toHaveBeenCalledWith(100, 200, 1, items);
+  });
+
+  it('should return smth', () => {
+    const f = jest.fn();
+
+    const items = [1, 2, 3];
+
+    items.reduce2(f);
+
+    expect(f).toHaveReturnedTimes(2);
+  });
+
+  it('should return smth', () => {
+    const f = jest.fn();
+
+    const items = [1, 2, 3];
+
+    items.reduce2(f, 2);
+
+    expect(f).toHaveReturnedTimes(3);
+  });
 });
